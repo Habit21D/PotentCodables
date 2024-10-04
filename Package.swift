2989dd8d1cdff205c068e06ed4e3832493f0de48
jinyuannan <jinyuannan@xiaomi.com>
2024-10-04 15:29:12 +0800

    fix: xcode18 编译失败

--- Package.swift
+++ Package.swift
@@ -28,7 +28,7 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/attaswift/BigInt.git", from: "5.0.0"),
 //    .package(url: "https://github.com/lukaskubanek/OrderedDictionary.git", from: "2.2.2")
-    .package(url: "https://github.com/Habit21D/OrderedDictionary.git", from: "2.2.2")
+    .package(url: "https://github.com/Habit21D/OrderedDictionary.git", from: "2.2.3")
   ],
   targets: [
     .target(
